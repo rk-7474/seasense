f6cb27ea8a04abb016aff933dea188f5a097ad87
Giulio <raggio271@gmail.com>
2025-02-06 12:40:54 +0100

    Moved and fixed custom logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Seasense",
   description: "Discover cutting-edge sea sensors to analyze essential ocean values such as carbon emissions, pH levels, and more.",
+  icons: {
+    icon: '/favicon.ico'
+  }
 };
 
 export default function RootLayout({
